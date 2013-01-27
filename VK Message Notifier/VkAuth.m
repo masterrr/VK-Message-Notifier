ebd0024770e61e67ec6f05168aca2d39675de36a
Dmitry Kurilo <me@masterrr.name>
2013-01-27 19:00:57 +0300

    TODO errorhandler clause, 10.7 deployment target

--- VK Message Notifier/VkAuth.m
+++ VK Message Notifier/VkAuth.m
@@ -8,23 +8,23 @@
 
 #import "VkAuth.h"
 
-
-
-
 @implementation VkAuth
 
 - (void)webView:(WebView*)sender didFinishLoadForFrame:(WebFrame*)frame {
-    // Gathering URL every time page loads
+    
     NSString *url = frame.dataSource.request.URL.absoluteString;
+    
     if ([url rangeOfString:@"access_token"].location != NSNotFound) {
-        // URL Parsing for token and user uid
         NSArray *urlcomponents = [url componentsSeparatedByString:@"="];
         _token = [[[urlcomponents objectAtIndex:1] componentsSeparatedByString:@"&"] objectAtIndex:0];
         _uid = [urlcomponents lastObject];
-        // calling - (void)didLogin in App.Deletage.h/m via loginProtocol
         if ([_delegate respondsToSelector:@selector(didLogin)]) {
             [_delegate didLogin];
         } 
+    } else {
+        if ([_delegate respondsToSelector:@selector(errorHandler)]) {
+            // TODO
+        }
     }
 }
 
